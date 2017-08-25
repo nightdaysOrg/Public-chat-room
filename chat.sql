141cb30589ace53174d7b9663f7eeb7a976fde13
hzCat <haizhoucat@qq.com>
2017-08-25 23:25:52 +0800

    更新最新版本

--- chat.sql
+++ chat.sql
@@ -2,5 +2,5 @@ create table chatroom(
     cid int primary key auto_increment,
     name varchar(32),
     chatinfo varchar(256),
-    chattime time
+    chattime datetime
 );
--- php/infoonpage.php
+++ php/infoonpage.php
@@ -1,7 +1,7 @@
 <?php
     header("Content-Type:application/json;charset=utf-8");
     require('init1.php');
-    $sql="select * from chatroom order by chattime desc LIMIT 50";
+    $sql="select * from chatroom order by cid desc LIMIT 50";
     $result=mysqli_query($conn,$sql);
     $posts=array();
     while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
